ce7181dfad3e0288224145ad72cc26f387ce1fa2
TheAliceBaskerville <the.garret.thief@gmail.com>
2024-08-23 23:25:22 +0400

    feat: add Piece class

---
+++ src/Piece.cpp
@@ -0,0 +1,36 @@
+#include "Piece.h"
+
+Piece::Piece(Color color, PieceType type) : color{color}, type{type}{}
+Piece::~Piece(){}
+
+Piece& Piece::operator=(const Piece& other){
+    color = other.color;
+    type = other.type;
+    return *this;
+}
+
+bool Piece::operator==(const Piece& other) const{
+    return (color == other.color && type == other.type);
+}
+
+bool Piece::operator!=(const Piece& other) const{
+    return !(*this == other);
+}
+
+PieceType Piece::getType() const{
+    return type;
+}
+
+Color Piece::getColor() const{
+    return color;
+}
+
+Piece& Piece::setType(PieceType type){
+    this->type = type;
+    return *this;
+}
+
+Piece& Piece::setColor(Color color){
+    this->color = color;
+    return *this;
+}
