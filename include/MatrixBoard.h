92a3f4c1b8e1c04d2e5b87d93304d8a35de360e8
TheAliceBaskerville <the.garret.thief@gmail.com>
2024-08-24 09:57:01 +0400

    feat(MatrixBoard, FEN): add MatrixBoard and initial FEN classes
    - Implement `MatrixBoard` class, which provides a matrix-based representation of the chessboard and implements the `IChessBoard` interface.
    - Introduce `FEN` class, currently in a rough state, used for initializing `MatrixBoard` only.
    
    The `FEN` class is expected to be completed in future updates to fully support FEN string parsing and validation.

---
+++ include/MatrixBoard.h
@@ -0,0 +1,64 @@
+#ifndef MATRIXBOARD_H
+#define MATRIXBOARD_H
+
+#include <vector>
+#include "IChessBoard.h"
+#include "FEN.h"
+
+class MatrixBoard : public IChessBoard{
+private:
+    class Square{
+    private:
+        bool isExistFlag, isEmptyFlag;
+        Piece piece; // Contains trash if (!isExistFlag || isEmptyFlag)
+    public:
+        Square(bool isExist=false);
+        Square(const Piece& piece);
+        ~Square();
+
+        bool isExist() const;
+        bool isEmpty() const;
+        const Piece& getPiece() const;
+        Square& setExist(bool isExistFlag);
+        Square& setEmpty(bool isEmptyFlag);
+        Square& setPiece(const Piece& piece);
+
+        bool operator==(const Square& other) const;
+        bool operator!=(const Square& other) const;
+    };
+private:
+    std::vector<std::vector<Square>> board;
+    int width, height;
+    // notationToIndex responsible for checking if given file and rank are valid.
+    std::pair<int, int> notationToIndex(int file, int rank) const;
+    std::pair<int, int> notationToIndex(const std::pair<int, int>& notation) const;
+public:
+    MatrixBoard(int height=8, int width=8);
+    MatrixBoard(const FEN& FEN);
+    MatrixBoard(const MatrixBoard& other);
+    MatrixBoard(MatrixBoard&& other) noexcept;
+    ~MatrixBoard() override;
+
+    MatrixBoard& operator=(const MatrixBoard& other);
+    MatrixBoard& operator=(MatrixBoard&& other) noexcept;
+
+    bool operator==(const MatrixBoard& other) const;
+    bool operator!=(const MatrixBoard& other) const;
+
+    int getWidth() const override;
+    int getHeight() const override;
+    std::pair<int, int> getSize() const override;
+
+    bool isExist(int file, int rank) const override;
+    bool isEmpty(int file, int rank) const override;
+    std::optional<Piece> getAt(int file, int rank) const override;
+
+    MatrixBoard& setAt(int file, int rank, const Piece& piece) override;
+    MatrixBoard& clearAt(int file, int rank) override;
+    MatrixBoard& removeAt(int file, int rank) override;
+    MatrixBoard& clear() override;
+
+    MatrixBoard& fromFEN(const FEN& FEN) override;
+};
+
+#endif
