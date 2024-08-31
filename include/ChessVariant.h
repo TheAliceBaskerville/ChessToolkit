4b085b9ff45c9da96b025f4fd3a00023800684c8
TheAliceBaskerville <the.garret.thief@gmail.com>
2024-08-31 20:52:25 +0400

    feat(ChessVariant): add `toString()` function for converting to `std::string`
    - Added `toString()` method to `ChessVariant` for converting enum values to their string representation.
    - `EnumConverter::toString()` throws an exception if an unexpected enum value is encountered.

--- include/ChessVariant.h
+++ include/ChessVariant.h
@@ -9,4 +9,17 @@ enum class ChessVariant{
     COUNT
 };
 
+namespace EnumConverter {
+    inline std::string toString(ChessVariant variant){
+        switch (variant) {
+            case ChessVariant::CLASSIC:  return "CLASSIC";
+            case ChessVariant::CHESS960: return "CHESS960";
+            case ChessVariant::MADHOUSE: return "MADHOUSE";
+            case ChessVariant::ATOMIC:   return "ATOMIC";
+            case ChessVariant::COUNT:    return "COUNT";
+            default: throw std::out_of_range{"Unexpected variant given."}; // TODO: think on better error message
+        }
+    }
+};
+
 #endif
